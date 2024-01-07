bea5111b120df6920a9e94f8401e8e3c8ea7832d
dmitrievaolga985 <146864409+dmitrievaolga985@users.noreply.github.com>
2024-01-07 18:07:00 +0400

    Add files via upload

---
+++ feeds.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<title>Animal</title>
+</head>
+<body>
+<h2 style="text-align: center;">Animals</h2>
+<blockquote>
+<p style="text-align: center;"><span style="text-decoration: underline;">Name_feeds</span></p>
+<p style="text-align: center;"><br /><img style="display: block; margin-left: auto; margin-right: auto;" src="https://www.bethowen.ru/upload/iblock/285/28503f7f2afb42d6d176a6287003229a.jpg" alt="" width="400" height="400" /></p>
+</blockquote>
+<p style="text-align: center;">Цена: price</p>
+<p style="text-align: justify;">Описание: opisanie</p>
+</body>
+</html>
